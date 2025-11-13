22a4eb702faadbb0c41cc8a5610fd21fa2c4a5d1
divya989178 <divya.beraboina@student.uantwerpen.be>
2025-11-13 10:44:44 +0100

    knn_demo_retrival.py

--- src/knn_demo_retrival.py
+++ src/knn_demo_retrival.py
@@ -1,7 +1,8 @@
 """
 calculates the similarity of paragraphs in each of these files with demo, 
 and gets indicies of top 5 paragraphs from demo similar to each paragraph in train,val and test set 
-and creates json files for train val and test with indices mapping to 5 similar paragraphs in demo
+and creates json files for train val and test with indices mapping to 5 similar paragraphs in demo.
+Output data is used for selecting examples for few shot
 """
 
 from tqdm import tqdm
